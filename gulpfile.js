a526c7139ee342256c44031cf522e3f11d465ff2
sundialmusic <paint@paint.local>
2015-05-21 15:29:10 -0700

    Adding Gulp, gitignore, json and shorter videos

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+  clean = require('gulp-clean'),
+  runSequence = require('run-sequence'),
+  filter = require('gulp-filter'),
+  sass = require('gulp-sass');
+
+gulp.task('clean', function() {
+  return gulp.src('dist', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('css', function() {
+  return gulp.src([
+      'sass/**/*.scss',
+      '!sass/**/_*.scss'
+    ])
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('build:css', ['clean'], function(cb) {
+  return runSequence('css', cb);
+});
+
+gulp.task('copy', function() {
+
+  return gulp.src([
+      '**/*',
+      '!sass/**/*',
+      // '!css/**/*.css',
+      '!dist/**/*',
+      '!node_modules/**/*',
+      '!gulpfile.js',
+      '!package.json',
+      '!.*'
+    ])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['clean'], function(cb) {
+  runSequence('copy', 'css', cb);
+});
+
+gulp.task('default', ['build']);
